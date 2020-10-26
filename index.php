87ae810c5e2b824cf784471b0444d96877ef494d
Arthur Jardim <62599485+Arthur2810@users.noreply.github.com>
2020-10-26 14:56:23 +0000

    CA1 - PHP project 1

--- index.php
+++ index.php
@@ -1,1 +1,17 @@
-<?php echo ('Hello World!'); ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My first PHP, CA1</title>
+</head>
+<body>
+    <div class= "jumbotron text-center" style="margin-bottom:0">
+    <h1>Web Development </h1>
+    <h2>CA1 - PHP project 1</h2>
+        <p>Arthur Jardim, Student number: 2019376</p>
+    
+    </div>
+    
+</body>
+</html>
